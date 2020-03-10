6f2317377470fb521d626576d5bbe91cbd031768
Alex Stout <alexstout009@hotmail.com>
2020-03-10 23:50:16 +0000

    refactored test structure

--- test/src/components/InputField.spec.jsx
+++ test/src/components/InputField.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import InputField from '../../src/components/InputField';
+import InputField from '../../../src/components/InputField';
 
 describe('InputField', () => {
   it('renders input field', () => {
