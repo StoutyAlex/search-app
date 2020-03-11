a96af4dc054dd0090f721da4b373e1f404f8f1b2
Alex Stout <alexstout009@hotmail.com>
2020-03-11 23:14:21 +0000

    Added SearchResults to index

--- src/components/SearchWidget.jsx
+++ src/components/SearchWidget.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 import InputField from './InputField';
+import SearchResults from './SearchResult';
 
-const SearchWidget = (props) => {
-  return (
-    <div className="c-search-widget">
-      <h2 className="c-search-widget__title">
-        Where are you going?
-      </h2>
-      <InputField
-        placeholder="city, airport, station, region and district..."
-        label="Pick-Up Location"
-      />
-    </div>
-  );
-};
+const SearchWidget = (props) => (
+  <div className="c-search-widget">
+    <h2 className="c-search-widget__title">
+      Where are you going?
+    </h2>
+    <InputField
+      placeholder="city, airport, station, region and district..."
+      label="Pick-Up Location"
+    />
+    <SearchResults />
+  </div>
+);
 
 export default SearchWidget;
