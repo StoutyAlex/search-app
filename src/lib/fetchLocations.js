92c4fd1d1cd1d7e019f9310a7801399e78a9873e
Alex Stout <alexstout009@hotmail.com>
2020-03-13 00:25:30 +0000

    created fetchLocations library and tests

--- src/lib/fetchLocations.js
+++ src/lib/fetchLocations.js
@@ -1,6 +1,25 @@
-import axios from 'axios';
+const axios = require('axios');
 
-export default async (url) => {
-  const response = await axios.get(url);
-  return response;
+const fetchLocations = async (searchTerm, itemsToReturn = 6) => {
+  const params = {
+    solrTerm: searchTerm,
+    solrIndex: 'fts_en',
+    solrRows: itemsToReturn,
+  };
+
+  let data;
+
+  try {
+    const response = await axios.get('https://www.rentalcars.com/FTSAutocomplete.do', {
+      params,
+    });
+
+    data = response.data.results.docs || [];
+  } catch (error) {
+    return [];
+  }
+
+  return data;
 };
+
+module.exports = fetchLocations;
--- test/src/lib/fetchLocations.spec.js
+++ test/src/lib/fetchLocations.spec.js
@@ -1,18 +1,73 @@
 import axios from 'axios';
 import fetchLocations from '../../../src/lib/fetchLocations';
 
+import locationResponse from '../../fixtures/locationResponse.json';
+
 jest.mock('axios');
 
 describe('fetchLocations', () => {
   beforeEach(() => {
     jest.resetAllMocks();
+    axios.get.mockResolvedValue(locationResponse);
   });
 
-  it('should make call to url', async () => {
-    const url = 'www.google.com';
+  it('should make get request to location endpoint', async () => {
+    await fetchLocations();
 
-    await fetchLocations(url);
+    const url = axios.get.mock.calls[0][0];
 
-    expect(axios.get).toHaveBeenCalledWith(url);
+    // Maybe put the url into config
+    expect(url).toEqual('https://www.rentalcars.com/FTSAutocomplete.do');
+  });
+
+  it('should add searchTerm into params', async () => {
+    const searchTerm = 'Manchester';
+
+    await fetchLocations(searchTerm);
+    const params = axios.get.mock.calls[0][1];
+
+    expect(params.params.solrTerm).toEqual(searchTerm);
+  });
+
+  it('should add solrIndex into params', async () => {
+    await fetchLocations('');
+    const params = axios.get.mock.calls[0][1];
+
+    expect(params.params.solrIndex).toEqual('fts_en');
+  });
+
+  it('should add solrRows into params - defaulted to 6', async () => {
+    await fetchLocations('');
+    const params = axios.get.mock.calls[0][1];
+
+    expect(params.params.solrRows).toBe(6);
+  });
+
+  it('should add custom solrRows value into params', async () => {
+    await fetchLocations('', 4);
+    const params = axios.get.mock.calls[0][1];
+
+    expect(params.params.solrRows).toBe(4);
+  });
+
+  it('should return docs array from response', async () => {
+    const result = await fetchLocations('');
+    expect(result).toBe(locationResponse.data.results.docs);
+  });
+
+  it('should return empty array when error fetching data', async () => {
+    axios.get.mockRejectedValue('failed');
+
+    const result = await fetchLocations('');
+
+    expect(result).toEqual([]);
+  });
+
+  it('should return empty array if data api changes', async () => {
+    axios.get.mockResolvedValue({ data: { results: { locations: [] } } });
+
+    const result = await fetchLocations('');
+
+    expect(result).toEqual([]);
   });
 });
