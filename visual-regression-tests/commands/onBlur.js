5f164ec2d89175ea61674739336ff4fde8e7bc22
Alex Stout <alexstout009@hotmail.com>
2020-03-16 02:13:40 +0000

    created pages

--- visual-regression-tests/commands/onBlur.js
+++
@@ -1,8 +0,0 @@
-module.exports.command = function(selector) {
-  this.execute(function(selector) {
-    const inputElement = document.querySelector(selector);
-    inputElement.blur();
-  }, [selector]);
-
-  return this;
-};
